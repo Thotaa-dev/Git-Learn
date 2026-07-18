33164eadb735011a7669082baef1d2fa386d1c3a
Thotaa-dev <mahenderthotaa@gmail.com>
2026-07-18 11:56:19 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -7,7 +7,6 @@ public class HelloWorld {
     }
     void sum(){
         System.out.println("Sum is");
-        System.out.println("Addition is");
     } void sub(){
         System.out.println("thota required");
 
