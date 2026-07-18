043e2e47b11e37987495443a44a6de17953fce84
Thota <mahenderthotaa@gmail.com>
2026-07-18 12:04:33 +0530

    Added

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Byee..........");
         System.out.println("Byee..........");
-        System.out.println("thota branch 1st update");
+        System.out.println("thota branch");
     }
     void sum(){
         System.out.println("Sum is");
