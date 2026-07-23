0acd0e999d1c59edfc751b90f91d4d653ad8073e
Thota <mahenderthotaa@gmail.com>
2026-07-23 09:32:44 +0530

    f

--- Hello.java
+++ Hello.java
@@ -8,7 +8,5 @@ public class HelloWorld {
     void thota(){
         System.out.println("Thota Branch");
         System.out.println("other dev update ");
-
-        System.out.println("my change update");
     }
 }
